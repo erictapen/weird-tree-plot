917f0238fb2f19f96aad063964c9aafb7d9068c5
Justin Humm <justin.humm@posteo.de>
2015-05-06 17:56:30 +0200

    minor changes

--- src/fileProcessing/SortedGraph.java
+++ src/fileProcessing/SortedGraph.java
@@ -1,5 +1,10 @@
 package fileProcessing;
 
+/** Import and Export functionality for .dot-files which are already sorted
+ * and are therefore faster readable
+ * @author justin
+ *
+ */
 public class SortedGraph {
 
 }
