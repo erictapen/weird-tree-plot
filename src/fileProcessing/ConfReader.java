b3cbc6d67e1bf08b3500d2f5653808749d858732
Justin Humm <justin.humm@posteo.de>
2015-06-18 15:22:59 +0200

    updated ConfReader

--- src/fileProcessing/ConfReader.java
+++ src/fileProcessing/ConfReader.java
@@ -1,23 +1,48 @@
 package fileProcessing;
 
-import plot.GraphPlotter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 
 public class ConfReader {
-
+	
+	/** Hashmap, which contains all Key/Value pairs in the configfile
+	 * 
+	 */
+	private HashMap<String, String> content;
+	
 	public ConfReader(String file) {
-		// TODO Auto-generated constructor stub
+		System.out.println("Reading config file " + file + " ...");
+		int count = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				count++;
+				if(line.startsWith("#")) continue;
+				if(line == "") continue;
+				String[] pair = line.split(" = ");
+				if(pair.length != 2) {
+					System.out.println("Syntax error on line " + count + 
+							"! There are only lines of the form \"KEY = VALUE\" allowed!");
+					continue;
+				}
+				content.put(pair[0], pair[1]);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Config file not found! Default values will be used.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("There was a problem with the config file.");
+			e.printStackTrace();
+		}
+		System.out.println(count + "lines of config read.");
 	}
 
-	/*
-	 * pltr.setRedrawInterval(200);
-		pltr.setMaxIteration(2000);
-		pltr.setStepsize(0.01);
-		pltr.setMovingCircleRadius(10.0);
-		pltr.setSizeOffSet(100000.0);
-		pltr.setMinNodeLeafs(1000);
-		pltr.getManager().setGridsize(0.0125);
-		pltr.setMinStepSizeBeforeAbort(0.02);
-		pltr.setPersistenceBeforeAbort(500);
-	 */
+	public String getValueByKey(String key) {
+		return content.get(key);
+	}
 
 }
