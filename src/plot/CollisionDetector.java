4be3ea11e824da9c3621b832b019fa15718c05b2
Justin Humm <justin.humm@posteo.de>
2015-07-16 13:57:56 +0200

    comments for CollisionDetector

--- src/plot/CollisionDetector.java
+++ src/plot/CollisionDetector.java
@@ -1,6 +1,14 @@
 package plot;
 
+/** This class is for parallelized collision detection. It will run as a seperate thread. Each thread grabs
+ * itself a portion of the movingNodes and checks it against all existing existing nodes. After every thread
+ * is ready, nodes are updated and the next iteration begins.
+ * @author justin
+ *
+ */
 public class CollisionDetector extends Thread{
+	
+	
 	
 	CollisionDetector() {
 		super();
@@ -9,5 +17,7 @@ public class CollisionDetector extends Thread{
 	public void run() {
 		
 	}
+	
+	
 	
 }
