628779d8966d99241a874497b7b852271e35cd79
erictapen <justin.humm@posteo.de>
2015-08-17 23:12:27 +0200

    changed my mind, new implementation

--- src/fileProcessing/ConfReader.java
+++ src/fileProcessing/ConfReader.java
@@ -22,10 +22,6 @@ public class ConfReader {
 	private char[] errorMsgBoolean;
 	
 	
-	private enum Value {
-	    INTEGER, STRING, DOUBLE, BOOLEAN
-	}
-	
 	public ConfReader(String file) {
 		this.content = new HashMap<String, String>();
 		System.out.println("Reading config file " + file + " ...");
@@ -53,75 +49,87 @@ public class ConfReader {
 		}
 		System.out.println(count + "lines of config read.");
 	}
-
-	private Object getValueByKey(String key, Value val, Object defaultValue) {
+	
+	private int getIntegerByKey(String key, int defaultValue) {
 		String str = this.content.get(key);
 		if(str==null) {
 			System.out.println("There was a problem with the attribute " + key 
-					+ ". Please check, if it fits the type " + val.toString() + ".");
+					+ ". Defaultvalue " + defaultValue + " will be used.");
 			return defaultValue;
 		} else {
-			switch(val) {
-				case INTEGER:
-					int resI = (int) defaultValue;
-					try{
-						resI = Integer.parseInt(str);
-					} catch(NumberFormatException e) {
-						System.out.println(this.errorMsgInteger);
-					}
-					return resI;
-				case DOUBLE:
-					double resD = (double) defaultValue;
-					try{
-						resD = Double.parseDouble(str);
-					} catch(NumberFormatException e) {
-						System.out.println(this.errorMsgDouble);
-					}
-					return resD;
-				case BOOLEAN:
-					boolean resB = (boolean) defaultValue;
-					try{
-						resB = Boolean.parseBoolean(str);
-					} catch(NumberFormatException e) {
-						System.out.println(this.errorMsgBoolean);
-					}
-					return resB;
-				case STRING:
-					return str;
+			int resI = (int) defaultValue;
+			try{
+				resI = Integer.parseInt(str);
+			} catch(NumberFormatException e) {
+				System.out.println("Error in " + key + "! " + str+ " is not an Integer!");
 			}
+			return resI;
+				
 		}
-		return null; //unreachable
+	}
+	
+	private double getDoubleByKey(String key, double defaultValue) {
+		String str = this.content.get(key);
+		if(str==null) {
+			System.out.println("There was a problem with the attribute " + key 
+					+ ". Defaultvalue " + defaultValue + " will be used.");
+			return defaultValue;
+		} else {
+			double resD = (double) defaultValue;
+			try{
+				resD = Double.parseDouble(str);
+			} catch(NumberFormatException e) {
+				System.out.println("Error in " + key + "! " + str+ " is not an Double!");
+			}
+			return resD;
+		}
+	}
+	
+	private boolean getBooleanByKey(String key, boolean defaultValue) {
+		String str = this.content.get(key);
+		if(str==null) {
+			System.out.println("There was a problem with the attribute " + key 
+					+ ". Defaultvalue " + defaultValue + " will be used.");
+			return defaultValue;
+		} else {
+			boolean resB = (boolean) defaultValue;
+			try{
+				resB = Boolean.parseBoolean(str);
+			} catch(NumberFormatException e) {
+				System.out.println("Error in " + key + "! " + str+ " is not an Boolean!");
+			}
+			return resB;
+		}
+	}
+
+	private String getStringByKey(String key, String defaultValue) {
+		String str = this.content.get(key);
+		if(str==null) {
+			System.out.println("There was a problem with the attribute " + key 
+					+ ". Defaultvalue " + defaultValue + " will be used.");
+			return defaultValue;
+		} else {
+			return str;
+		}
 	}
 	
 	public void setupGraphDraw(GraphDraw obj) {
-		int sizex = (int)this.getValueByKey("DRAWdisplayWidth", Value.INTEGER, 512);
-		int sizey = (int)this.getValueByKey("DRAWdisplayHeight", Value.INTEGER, 512);
+		int sizex = this.getIntegerByKey("DRAWdisplayWidth", 512);
+		int sizey = this.getIntegerByKey("DRAWdisplayHeight", 512);
 		if(sizey==0) sizey = sizex; //this is specified in plotter_example.conf
 		obj.size(sizex, sizey);
 		
-		obj.setInputDOTfile((String)this.getValueByKey("GRAPHinputDOTfile", Value.STRING, "data/input.dot"));
+		obj.setInputDOTfile(this.getStringByKey("GRAPHinputDOTfile", "data/input.dot"));
 		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
+		obj.setRootCaption(this.getStringByKey("GRAPHrootCaption", "Philosophie"));
 		
-		obj.setExportfile((String)this.getValueByKey("GRAPHoutputTEXfile", Value.STRING, "out/out.tex"));
+		obj.setExportfile(this.getStringByKey("GRAPHoutputTEXfile", "out/out.tex"));
 		
-		obj.setDrawRootSize((double)this.getValueByKey("DRAWrootSize", Value.DOUBLE, 75.0));
+		obj.setDrawRootSize(this.getDoubleByKey("DRAWrootSize", 75.0));
 		
-		obj.setDrawLines((boolean)this.getValueByKey("DRAWlines", Value.BOOLEAN, false));
+		obj.setDrawLines(this.getBooleanByKey("DRAWlines", false));
 		
-		obj.setDrawEveryUpdateInterval((int)this.getValueByKey("DRAWeveryNumberOfUpdates", Value.INTEGER, 10));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
-		
-		obj.setRootCaption((String)this.getValueByKey("GRAPHrootCaption", Value.STRING, "Philosophie"));
+		obj.setDrawEveryUpdateInterval(this.getIntegerByKey("DRAWeveryNumberOfUpdates", 10));
 		
 		
 	}
