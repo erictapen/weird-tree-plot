a79f8563c809761f8cbb6692425b38f56c993c19
erictapen <justin.humm@posteo.de>
2015-08-23 19:49:35 +0200

    command line arguments are passed to the sketch

--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -1,10 +1,12 @@
+import org.apache.commons.lang3.ArrayUtils;
+
 import processing.core.PApplet;
 
 
 public class EntryPoint {
 
-	public static void main(String args[]) {
-		PApplet.main(new String[] { "draw.GraphDraw" });
+	public static void main(String[] args) {
+		PApplet.main(ArrayUtils.addAll(new String[] { "draw.GraphDraw"}, args));
 	}
 
 }
