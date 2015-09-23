abadae5ba5f8ec8339716a04c7823016b7dd1021
erictapen <justin.humm@posteo.de>
2015-09-23 02:11:45 +0200

    added file DrawableGraph

---
+++ src/fileProcessing/DrawableGraph.java
@@ -0,0 +1,11 @@
+package fileProcessing;
+
+/**If you want to draw yout plot on paper, you will get some problems with small node sizes. This classes aproach is, 
+ * to divide all nodes into three kinds, depending on node size:
+ * 
+ * @author justin
+ *
+ */
+public class DrawableGraph {
+
+}
